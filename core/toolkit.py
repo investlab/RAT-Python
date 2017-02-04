5aa957b94a7a601b4a0b0a6fa1a7a029cc11a028
vesche <austinjackson892@gmail.com>
2017-02-03 23:58:38 -0500

    Remove junk from toolkit module.

--- core/toolkit.py
+++ core/toolkit.py
@@ -11,23 +11,6 @@ import urllib
 import zipfile
 
 
-# def id(platform):
-#    return 'foo'
-
-
-# def screenshot(platform):
-#    if platform.startswith('win') or platform.startswith('darwin'):
-#        from PIL import ImageGrab
-#    elif platform.startswith('linux'):
-#        return
-#    else:
-#        return 'Platform unsupported.'
-
-
-# def sysinfo(platform):
-#     return 'bar'
-
-
 def unzip(f):
     if os.path.isfile(f):
         try:
